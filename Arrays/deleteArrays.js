715cf28160ca0964e31d9ed2eb12e612604628bc
Akanle Jesus <jesuswrites20043@gmail.com>
2023-07-10 15:23:19 +0100

    Update deleteArrays.js

--- Arrays/deleteArrays.js
+++ Arrays/deleteArrays.js
@@ -1,1 +1,26 @@
 
+function deleteArray(array, index) {
+  // Check if the index is valid.
+  if (index < 0 || index >= array.length) {
+    return;
+  }
+
+  // Shift all the elements after the index to the left.
+  for (let i = index; i < array.length - 1; i++) {
+    array[i] = array[i + 1];
+  }
+
+  // Decrease the length of the array by 1.
+  array.length -= 1;
+}
+
+const array = [1, 2, 3, 4, 5];
+
+// Delete the element at index 2.
+deleteArray(array, 2);
+
+console.log(array); // [1, 2, 4, 5]
+
+
+
+
